11c04f1a01e2ea6eb5145b9e5af12ee368ed047f
Crat K <eorud196@gmail.com>
2024-09-26 17:47:07 +0900

    1. 어빌리티 시스템 라이브러리 생성중

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/WidgetController/UIWidgetController.h"
+#include "UI/HUD/AuraHUD.h"
+#include "Player/AuraPlayerState.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	// 로컬 플레이어의 컨트롤러 가져오기
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+
+			const FWidgetControllerParams Params(PC, PS, ASC, AS);
+
+			return AuraHUD->GetOverlayWidgetController(Params);
+		}
+	}
+	return nullptr;
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintPure, category="AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
